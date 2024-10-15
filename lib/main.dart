bdb3e349ee07105d47d9a8aadb1715ac7515a6bb
尾本悦基 <tailbook_etu2712@omotoetsumotonoMacBook-Pro.local>
2024-10-16 00:19:01 +0900

    APIキーの秘匿化

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:walk_tracker_app/walking_tracker_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env"); // .envファイルを読み込む
   await Firebase.initializeApp();
   runApp(MyApp());
 }
