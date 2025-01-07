53c360eba1db47b47cb47cde19f26f56f25e8bf9
尾本悦基 <tailbook_etu2712@omotoetsumotonoMacBook-Pro.local>
2025-01-07 17:36:05 +0900

    flutter_backgrouundgeolocationでの軌跡の記録に戻す

--- lib/walking_tracker_screen.dart
+++ lib/walking_tracker_screen.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:flutter_background_geolocation/flutter_background_geolocation.dart'
+    as bg;
 import 'package:path_provider/path_provider.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:walk_tracker_app/artwork_list_screen.dart';
@@ -52,6 +54,7 @@ class _WalkingTrackerScreenState extends State<WalkingTrackerScreen> {
     _initializeScreen(); // 非同期初期化処理をまとめたメソッド
     _loadSavedArtworks();
     _checkPermissionAndStartTracking();
+    _initializeBackgroundGeolocation();
     _setInitialCameraPosition();
 
     // ポリラインを初期化
@@ -85,25 +88,22 @@ class _WalkingTrackerScreenState extends State<WalkingTrackerScreen> {
     final artworkFiles = artworksDirectory.listSync().whereType<File>();
     final canvasFiles = canvasDirectory.listSync().whereType<File>();
 
-    savedArtworks = artworkFiles
-        .map((artworkFile) {
-          // 対応するキャンバス状態ファイルを取得
-          final timestamp = artworkFile.path.split('_').last.split('.').first;
-          final canvasFile = canvasFiles.firstWhere(
-            (file) => file.path.contains(timestamp),
-            orElse: () => File(''), // 見つからなければ空ファイル（スキップ）
-          );
+    savedArtworks = artworkFiles.map((artworkFile) {
+      // 対応するキャンバス状態ファイルを取得
+      final timestamp = artworkFile.path.split('_').last.split('.').first;
+      final canvasFile = canvasFiles.firstWhere(
+        (file) => file.path.contains(timestamp),
+        orElse: () => File(''), // 見つからなければ空ファイル（スキップ）
+      );
 
-          if (canvasFile.existsSync()) {
-            return {
-              'imageFile': artworkFile,
-              'canvasFile': canvasFile,
-            };
-          }
-          return null; // キャンバスファイルがない場合スキップ
-        })
-        .whereType<Map<String, dynamic>>()
-        .toList();
+      if (canvasFile.existsSync()) {
+        return {
+          'imageFile': artworkFile,
+          'canvasFile': canvasFile,
+        };
+      }
+      return null; // キャンバスファイルがない場合スキップ
+    }).whereType<Map<String, dynamic>>().toList();
   }
 
   // 位置情報の許可を確認してトラッキングを開始
@@ -129,20 +129,7 @@ class _WalkingTrackerScreenState extends State<WalkingTrackerScreen> {
     setState(() {
       _isTracking = true;
     });
-
-    int distanceFilter = 5; // 初期の距離フィルタ(5m)
-    // geolocatorのストリーミングで位置情報を取得
-    _positionStream = Geolocator.getPositionStream(
-      locationSettings: LocationSettings(
-        accuracy: LocationAccuracy.high,
-        distanceFilter: distanceFilter, // 5mごとに位置情報を取得
-      ),
-    ).listen((Position position) {
-      if (_isRecording) {
-        _checkIfUserIsMoving(position);
-        _adjustTrackingInterval(position.speed);
-      }
-    });
+    bg.BackgroundGeolocation.start();
   }
 
   // 位置情報の記録を開始
@@ -198,7 +185,7 @@ class _WalkingTrackerScreenState extends State<WalkingTrackerScreen> {
     final currentDate = DateTime.now().toIso8601String();
     // generateGroupId を使って groupId を生成
     String groupId = generateGroupId(_positions);
-
+  
     await _dbHelper.insertWalkingData(
       groupId: groupId,
       date: currentDate,
@@ -221,6 +208,39 @@ class _WalkingTrackerScreenState extends State<WalkingTrackerScreen> {
     _positions.clear();
   }
 
+  // BackgroundGeolocationの初期化
+  void _initializeBackgroundGeolocation() {
+    bg.BackgroundGeolocation.onLocation((bg.Location location) {
+      _checkIfUserIsMoving(Position(
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        timestamp: DateTime.now(),
+        accuracy: location.coords.accuracy,
+        altitude: location.coords.altitude,
+        altitudeAccuracy: location.coords.altitudeAccuracy ?? 0.0,
+        heading: location.coords.heading,
+        speed: location.coords.speed,
+        speedAccuracy: location.coords.speedAccuracy ?? 0.0,
+        headingAccuracy: location.coords.headingAccuracy ?? 0.0,
+      ));
+    }, (bg.LocationError error) {
+      print("[onLocation] ERROR: ${error.code}, ${error.message}");
+    });
+
+    // BackgroundGeolocationの設定
+    bg.BackgroundGeolocation.ready(bg.Config(
+      desiredAccuracy: bg.Config.DESIRED_ACCURACY_HIGH,
+      distanceFilter: 5.0,
+      stopOnTerminate: false,
+      startOnBoot: true,
+      enableHeadless: true,
+    )).then((bg.State state) {
+      if (!state.enabled) {
+        bg.BackgroundGeolocation.start();
+      }
+    });
+  }
+
   // ユーザーが移動しているかどうかをチェック
   Future<void> _checkIfUserIsMoving(Position newPosition) async {
     if (_previousPosition != null) {
@@ -231,102 +251,100 @@ class _WalkingTrackerScreenState extends State<WalkingTrackerScreen> {
         newPosition.longitude,
       );
 
-      // ノイズチェック
       if (_isNoise(distance)) {
-        return; // ノイズは無視
+        return;
       }
 
-      // 記録中の場合、位置情報を追加
+      // 移動中の場合、速度に応じて記録間隔を変更
+      int recordingInterval;
+      if (newPosition.speed < 1.5) {
+        // ゆっくり歩行
+        recordingInterval = 7000;
+      } else if (newPosition.speed < 3.5) {
+        // 速歩
+        recordingInterval = 5000;
+      } else if (newPosition.speed < 10.0) {
+        // 走る
+        recordingInterval = 3000;
+      } else {
+        // さらに早い場合
+        recordingInterval = 2000;
+      }
+      
+      // 記録中の場合、位置情報を記録
       if (_isRecording) {
-        _updateDistance(newPosition); // 累積距離を更新
+        _positions.add(newPosition);
+
+        // ポリラインを更新
+        _polylines = {
+          Polyline(
+            polylineId: PolylineId("current_route"),
+            points: _positions
+                .map((pos) => LatLng(pos.latitude, pos.longitude))
+                .toList(),
+            color: Colors.blue,
+            width: 5,
+          ),
+        };
+        setState(() {});
+
+        await Future.delayed(Duration(milliseconds: recordingInterval));
       }
 
-      // ユーザの移動状態の更新
-      setState(() {
-        _isMoving = newPosition.speed > speedThreshold;
-        _currentPosition = newPosition;
-      });
+      if (newPosition.speed > speedThreshold) {
+        setState(() {
+          _isMoving = true;
+          _currentPosition = newPosition;
+        });
+      } else {
+        setState(() {
+          _isMoving = false;
+        });
+      }
     } else {
-      _currentPosition = newPosition; // 初回の位置設定
+      _currentPosition = newPosition;
     }
+
     _previousPosition = newPosition;
   }
 
-  // ノイズの判定
   bool _isNoise(double distance) {
-    return distance < noiseThreshold; // 2m以下をノイズとして判定
+    return distance < noiseThreshold;
   }
 
   // 位置情報の距離を更新
   void _updateDistance(Position newPosition) {
     if (_previousPosition != null) {
-      final double distance = Geolocator.distanceBetween(
+      final distance = Geolocator.distanceBetween(
         _previousPosition!.latitude,
         _previousPosition!.longitude,
         newPosition.latitude,
         newPosition.longitude,
       );
-
-      // ノイズを無視し倍場合のみ距離を累積
-      if (distance > noiseThreshold) {
-        _totalDistance += distance;
-
-        // ポリラインを更新
-        setState(() {
-          _positions.add(newPosition);
-          _polylines = {
-            Polyline(
-              polylineId: PolylineId("current_route"),
-              points: _positions
-                  .map((pos) => LatLng(pos.latitude, pos.longitude))
-                  .toList(),
-              color: Colors.blue,
-              width: 5,
-            ),
-          };
-        });
-      }
+      _totalDistance += distance;
     }
 
-    _previousPosition = newPosition;
-  }
-
-  void _adjustTrackingInterval(double speed) {
-    int newDistanceFilter;
-
-    if (speed < 1.5) {
-      newDistanceFilter = 7; // ゆっくり歩行（7mごとに取得）
-    } else if (speed < 3.5) {
-      newDistanceFilter = 5; // 速歩（5mごとに取得）
-    } else if (speed < 10.0) {
-      newDistanceFilter = 3; // 走る（3mごとに取得）
-    } else {
-      newDistanceFilter = 2; // 速い移動（2mごとに取得）
-    }
-
-    // 現在の設定が異なる場合のみ再設定
-    if (_positionStream != null) {
-      _positionStream?.cancel(); // 既存のストリームを停止
-
-      _positionStream = Geolocator.getPositionStream(
-        locationSettings: LocationSettings(
-          accuracy: LocationAccuracy.high,
-          distanceFilter: newDistanceFilter,
+    // ポリラインを更新
+    setState(() {
+      _positions.add(newPosition);
+      _polylines = {
+        Polyline(
+          polylineId: PolylineId("current_route"),
+          points: _positions
+              .map((pos) => LatLng(pos.latitude, pos.longitude))
+              .toList(),
+          color: Colors.blue,
+          width: 5,
         ),
-      ).listen((Position position) {
-        if (_isRecording) {
-          _checkIfUserIsMoving(position);
-          _adjustTrackingInterval(position.speed); // 再度速度をチェック
-        }
-      });
-    }
+      };
+    });
+    _previousPosition = newPosition;
   }
 
   // Firestoreからデータを復元
   Future<void> _restoreDataFromFirestore() async {
     try {
-      List<Map<String, dynamic>> allWalkingData =
-          await _firestoreService.getAllWalkingData();
+      List<Map<String, dynamic>> allWalkingData = await _firestoreService.getAllWalkingData();
       for (var data in allWalkingData) {
         // 各フィールドを取得
         String groupId = data['groupId'] ?? 0;
@@ -336,8 +354,7 @@ class _WalkingTrackerScreenState extends State<WalkingTrackerScreen> {
 
         // positionsがリスト型かを確認
         if (data['positions'] is List) {
-          List<Map<String, dynamic>> positions =
-              List<Map<String, dynamic>>.from(data['positions']);
+          List<Map<String, dynamic>> positions = List<Map<String, dynamic>>.from(data['positions']);
 
           // データをローカルデータベースに保存
           await _dbHelper.insertWalkingData(
@@ -360,8 +377,7 @@ class _WalkingTrackerScreenState extends State<WalkingTrackerScreen> {
   // ローカルデータベースから軌跡を読み込む
   Future<void> _loadTrajectories() async {
     try {
-      List<Map<String, dynamic>> walkingData =
-          await _dbHelper.getAllWalkingData();
+      List<Map<String, dynamic>> walkingData = await _dbHelper.getAllWalkingData();
       List<List<Position>> loadedTrajectories = [];
 
       for (var data in walkingData) {
@@ -419,8 +435,7 @@ class _WalkingTrackerScreenState extends State<WalkingTrackerScreen> {
 
   @override
   void dispose() {
-    _positionStream?.cancel(); // Geolocatorのストリームを停止
-    _stepStream?.cancel();
+    bg.BackgroundGeolocation.stop();
     super.dispose();
   }
 
@@ -472,11 +487,11 @@ class _WalkingTrackerScreenState extends State<WalkingTrackerScreen> {
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.brush),
-            label: 'アートの作成',
+            label: '作品の制作',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.collections),
-            label: '絵の閲覧',
+            label: 'ギャラリー',
           ),
         ],
         onTap: (index) {
