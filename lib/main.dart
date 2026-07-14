2173b828a201d3c89fe9fdc049fa63cbb460cef8
尾本悦基 <tailbook_etu2712@omotoetsumotonoMacBook-Pro.local>
2026-07-14 15:54:32 +0900

    androidでのappbar表記を左揃えからcenterに変更

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'GPStroke',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(centerTitle: true),
+      ),
       home: _RootScreen(showOnboarding: showOnboarding),
     );
   }
